7cdd9f41ad7e7ba30e7b79f641c41c46fb1e5c05
Viktor Riga <rigavv@gmail.com>
2020-05-18 11:27:18 +0300

    start

---
+++ Assignment-4.-CoffeMachine/Assignment-4.-CoffeMachine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment-4.-CoffeMachine
+//
+//  Created by Viktor Riga on 18.05.2020.
+//  Copyright © 2020 Viktor Riga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
